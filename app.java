a1d639cf55467aee2e9c5eca0d1fccf9afa256f8
GRatna123 <140251297+GRatna123@users.noreply.github.com>
2025-08-15 17:52:32 +0530

    Update app.java

--- app.java
+++ app.java
@@ -1,13 +1,31 @@
-public class VowelConsonant {
+// Java Program to Swap Two values using third variable
+// using temp variable 
 
-    public static void main(String[] args) {
+// Importing generic libraries
+import java.util.*;
 
-        char ch = 'i';
+class GFG {
 
-        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
-            System.out.println(ch + " is vowel");
-        else
-            System.out.println(ch + " is consonant");
+    // Function to swap two numbers
+    // Using temporary variable
+    static void swapValuesUsingThirdVariable(int m, int n)
+    {
+        // Swapping the values
+        int temp = m;
+        m = n;
+        n = temp;
+        System.out.println("Value of m is " + m
+                           + " and Value of n is " + n);
+    }
 
+    // Main driver code
+    public static void main(String[] args)
+    {
+        // Random integer values
+        int m = 9, n = 5;
+
+        // Calling above function to
+        // reverse the numbers
+        swapValuesUsingThirdVariable(m, n);
     }
 }
